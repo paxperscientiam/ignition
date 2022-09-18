143f90cc8668ee072a44b41ea3651f477c168f9c
Chris <7539871+paxperscientiam@users.noreply.github.com>
2022-09-18 12:40:47 -0700

    add option to return string of output

--- src/ErrorPage/Renderer.php
+++ src/ErrorPage/Renderer.php
@@ -9,12 +9,17 @@ class Renderer
      *
      * @return void
      */
-    public function render(array $data): void
+    public function render(array $data, bool $return = false): void|string
     {
         $viewFile = __DIR__ . '/../../resources/views/errorPage.php';
 
         extract($data, EXTR_OVERWRITE);
 
+        if (true === $return) {
+            ob_start();
+            include $viewFile;
+            return ob_get_clean();
+        }
         include $viewFile;
     }
 }
