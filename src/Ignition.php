02d0037b4394b24c2624cef12b2c666f223a9456
Chris <7539871+paxperscientiam@users.noreply.github.com>
2022-09-18 12:48:21 -0700

    Update Ignition.php

--- src/Ignition.php
+++ src/Ignition.php
@@ -320,7 +320,7 @@ class Ignition
             $this->customHtmlBody
         );
 
-        (new Renderer())->render(['viewModel' => $viewModel]);
+        (new Renderer())->render(['viewModel' => $viewModel, 'returnAsString' => $this->returnAsString]);
     }
 
     /**
